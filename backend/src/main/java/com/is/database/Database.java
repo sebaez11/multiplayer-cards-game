74a8cca082c9110c48b464221fc4336241f1d6b1
Sebastian Baez <sebaez200011@gmail.com>
2022-03-05 11:56:31 -0500

    Database connection created

--- backend/src/main/java/com/is/database/Database.java
+++ backend/src/main/java/com/is/database/Database.java
@@ -4,10 +4,44 @@
  */
 package com.is.database;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 /**
  *
  * @author sebastian-dev
  */
 public class Database {
+    
+    private static Connection connection = null;
+
+    public static Connection getConecction() throws SQLException {
+
+        String ip = "localhost";
+        String basedatos = "playcards_DB";
+        String usuario = "postgres";
+        String pass = "postgres";
+        String puerto = "5432";
+        String url = "jdbc:postgresql://" + ip + ":" + puerto + "/" + basedatos;
+
+        try {
+            if (Database.connection == null) {
+                Class.forName("org.postgresql.Driver");
+                Database.connection = DriverManager.getConnection(url, usuario, pass);
+                Database.connection.setAutoCommit(false);
+                return Database.connection;
+            }
+        } catch (Exception ex) {
+            System.out.println("ERROR EN EL DRIVER \n ERROR : " + ex.getMessage());
+            Logger.getLogger(Database.class.getName()).log(Level.SEVERE, null, ex);
+            Database.connection.close();
+        }
+
+        return Database.connection;
+
+    }
     
 }
