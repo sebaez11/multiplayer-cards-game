9b509d0961386fd297e66c7f4786c387b4d4a6c0
Brian Arias <brian.arias@unillanos.edu.co>
2022-03-05 04:42:14 -0500

    User model was created

--- backend/src/main/java/com/is/models/Users.java
+++ backend/src/main/java/com/is/models/Users.java
@@ -4,10 +4,57 @@
  */
 package com.is.models;
 
-/**
- *
- * @author sebastian-dev
- */
 public class Users {
+    int id,role_id;
+    String username, email, password;
+
+    public Users(int id, int role_id, String username, String email, String password) {
+        this.id = id;
+        this.role_id = role_id;
+        this.username = username;
+        this.email = email;
+        this.password = password;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setRole_id(int role_id) {
+        this.role_id = role_id;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public int getRole_id() {
+        return role_id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+    
     
 }
