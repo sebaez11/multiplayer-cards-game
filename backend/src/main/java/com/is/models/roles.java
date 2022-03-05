e546929efcd2053a60bdcd86ae00a1cadf0d9f98
Brian Arias <brian.arias@unillanos.edu.co>
2022-03-05 04:48:39 -0500

    roles model was created

--- backend/src/main/java/com/is/models/roles.java
+++ backend/src/main/java/com/is/models/roles.java
@@ -9,5 +9,29 @@ package com.is.models;
  * @author drago
  */
 public class roles {
+    int id;
+    String name;
+
+    public roles(int id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
     
+   
 }
