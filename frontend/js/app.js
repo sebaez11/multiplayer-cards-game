32deb2cb41c978e35c1cee76e97e652e534bb48c
Diego2 <diego2@contabilidad.com>
2022-03-09 21:28:50 -0500

    add modals cards and games

---
+++ frontend/js/app.js
@@ -0,0 +1,6 @@
+const modalCreateButton = document.getElementById('create-game-button');
+const createModal = document.getElementById('createModal');
+// modalCreateButton.addEventListener('shown.bs.modal', () => {
+//   createModal.style.display = 'block';
+//   createModal.style.opacity = 1;
+// });
